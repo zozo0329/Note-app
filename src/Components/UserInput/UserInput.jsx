0c3034b8e4ddf9571732461f551f9ac6903dba6f
Zo <161946731+zozo0329@users.noreply.github.com>
2024-12-08 09:39:16 +0800

    1st Commit

---
+++ src/Components/UserInput/UserInput.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserInput = () => {
+  return (
+    <div className="UserInput-container">
+      <form>
+        <div className="input-container">
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UserInput;
